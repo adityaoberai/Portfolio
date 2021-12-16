9dc074afab7bbe397bd978ab10208d77c460fac3
Aditya Oberai <adityaoberai1@gmail.com>
2021-12-17 01:06:32 +0530

    Correct endpoints

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-app.use(express.static('./');
+app.use(express.static('./'));
 
 app.get('/', function(req, res) {
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
@@ -13,42 +13,42 @@ app.get('/', function(req, res) {
 
 app.get('/index', function(req, res) {
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join('index.html'));
 });
 
 app.get('/about', function(req, res) {
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.sendFile(path.join(__dirname, 'about.html'));
+    res.sendFile(path.join('about.html'));
 });
 
 app.get('/blogs', function(req, res) {
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.sendFile(path.join(__dirname, 'blogs.html'));
+    res.sendFile(path.join('blogs.html'));
 });
 
 app.get('/projects', function(req, res) {
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.sendFile(path.join(__dirname, 'projects.html'));
+    res.sendFile(path.join('projects.html'));
 });
 
 app.get('/contact', function(req, res) {
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.sendFile(path.join(__dirname, 'contact.html'));
+    res.sendFile(path.join('contact.html'));
 });
 
 app.get('/linkedin', function(req, res) {
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.sendFile(path.join(__dirname, 'Socials/linkedin.html'));
+    res.sendFile(path.join('Socials/linkedin.html'));
 });
 
 app.get('/twitter', function(req, res) {
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.sendFile(path.join(__dirname, 'Socials/twitter.html'));
+    res.sendFile(path.join('Socials/twitter.html'));
 });
 
 app.get('/resume', function(req, res) {
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.sendFile(path.join(__dirname, 'Socials/resume.html'));
+    res.sendFile(path.join('Socials/resume.html'));
 });
 
 module.exports = app;
